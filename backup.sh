fc727ab39f4d32a12446842846a7b652e06c7ac6
rijuvenator <jujudg1031@gmail.com>
2015-10-01 20:47:09 +0200

    removed dots; implementation updates

--- backup.sh
+++ backup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cp ~/.vimrc ~/settings/.vimrc_mac
-cp ~/.bash_profile ~/settings/.bash_profile_mac
-scp adasgupt@lxplus.cern.ch:~/.bash_profile ~/settings/.bash_profile_lxplus
-scp adasgupt@lxplus.cern.ch:~/.vimrc ~/settings/.vimrc_lxplus
+cp ~/.vimrc ~/settings/vimrc_mac
+cp ~/.bash_profile ~/settings/bash_profile_mac
+rm -r ~/settings/vim_mac/
+cp -r ~/.vim ~/settings/vim_mac
+echo "Copying from lxplus; you'll be asked for your password."
+scp adasgupt@lxplus.cern.ch:~/".bash_profile .vimrc" ~/settings/
+mv .bash_profile bash_profile_lxplus
+mv .vimrc vimrc_lxplus
