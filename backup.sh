8bdf4ed36c322e58a9dc189a1a6a7a331449b59f
rijuvenator <jujudg1031@gmail.com>
2015-10-02 11:58:56 +0200

    changed to .sh for syntax highlighting

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp ~/.vimrc ~/settings/vimrc_mac
-cp ~/.bash_profile ~/settings/bash_profile_mac
+cp ~/.vimrc ~/settings/vimrc_mac.sh
+cp ~/.bash_profile ~/settings/bash_profile_mac.sh
 rm -r ~/settings/vim_mac/
 cp -r ~/.vim ~/settings/vim_mac
 echo "Copying from lxplus; you'll be asked for your password."
 scp adasgupt@lxplus.cern.ch:~/".bash_profile .vimrc .rootlogon.C" ~/settings/
-mv ~/settings/.bash_profile ~/settings/bash_profile_lxplus
-mv ~/settings/.vimrc ~/settings/vimrc_lxplus
+mv ~/settings/.bash_profile ~/settings/bash_profile_lxplus.sh
+mv ~/settings/.vimrc ~/settings/vimrc_lxplus.sh
 mv ~/settings/.rootlogon.C ~/settings/rootlogon.C
