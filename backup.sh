8c98f1e620d7a18407c8d4bac567e03ea74209b0
rijuvenator <jujudg1031@gmail.com>
2015-10-16 14:30:04 +0200

    changed vimrc to .vim for syntax

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp ~/.vimrc ~/settings/vimrc_mac.sh
+cp ~/.vimrc ~/settings/vimrc_mac.vim
 cp ~/.bash_profile ~/settings/bash_profile_mac.sh
 rm -r ~/settings/vim_mac/
 cp -r ~/.vim ~/settings/vim_mac
 echo "Copying from lxplus; you'll be asked for your password."
 scp adasgupt@lxplus.cern.ch:~/".bash_profile .vimrc .rootlogon.C" ~/settings/
 mv ~/settings/.bash_profile ~/settings/bash_profile_lxplus.sh
-mv ~/settings/.vimrc ~/settings/vimrc_lxplus.sh
+mv ~/settings/.vimrc ~/settings/vimrc_lxplus.vim
 mv ~/settings/.rootlogon.C ~/settings/rootlogon.C
