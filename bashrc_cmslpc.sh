af0ea9d887545f080d7dc21e1712cbf66922e2d2
rijuvenator <jujudg1031@gmail.com>
2016-02-11 19:16:31 +0100

    overhauled ls colors adding several filetypes

--- bashrc_cmslpc.sh
+++ bashrc_cmslpc.sh
@@ -5,8 +5,11 @@
 
 # Sets time to orange, username@hostname to green, full directory to red, commands to blue, everything except commands to bold
 export PS1="\[\e[1;38;5;166m\][\t] \[\e[38;5;28m\]\u@\h: \[\e[31m\]\w\$ \[\e[0;34m\]"
-# Sets directories to bblue, links to bcyan, executables to bmagenta
-LS_COLORS="di=1;34:ln=1;36:ex=1;31"
+# Colors output of ls. Bold means not text, don't open in a text editor.
+# directories are blue, links are cyan, executables are red, graphics files are green,
+# C files are purple, header files are orange, python files are teal, root files are light blue,
+# archive files are pink
+LS_COLORS="di=1;34:ln=1;36:ex=1;91:*.eps=1;32:*.pdf=1;32:*.png=1;32:*.jpg=1;32:*.cc=38;5;90:*.C=38;5;90:*.cpp=38;5;90:*.h=38;5;166:*.hh=38;5;166:*.py=38;5;30:*.pyc=1;38;5;30:*.root=1;38;5;33:*.tar=1;38;5;201:*.tgz=1;38;5;201:*.gz=1;38;5;201:*.zip=1;38;5;201:*.7z=1;38;5;201"
 
 # Resets color before output!
 trap 'echo -ne "\e[0m"' DEBUG
